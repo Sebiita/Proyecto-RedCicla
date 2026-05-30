c68279daddfd6f1a5936d91d745ca4a4e220ed23
Nicolas-Urbina <nurbina24@alumnos.utalca.cl>
2026-05-30 11:43:13 -0400

    "primer comit yeeeeeei 1.- cree el app.py para levantar el servidor todavia no tiene conexion con las rutas pero vamos de apoco cree el data.json cree el requiremets y comienzo con el routes_usuarios.py que se va a dedicar a tener la info para iniciar sesion de los usuairos, leer un usuario, crear un usuario y eliminar"

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"mensaje": "Servidor FastAPI funcionando"}
+
+
+
+
+
+
+
+# Comandos útiles:
+# Iniciar servidor: python -m uvicorn app:app --reload
+# Iniciar servidor LAN público:
+#     python -m uvicorn app:app --host 0.0.0.0 --port 8000
+# Abrir chatbot:
+#     http://127.0.0.1:8000/static/chat_bot/chat_bot.html
+# Migrar datos: python scripts\migracion_sqlite.py
+# Consulta directa:
+#     python scripts\consultas_sqlite.py --query
+#     "SELECT * FROM usuarios WHERE ciudad IS NOT NULL"
+# Para activar el entrono virtual: source .venv/bin/activate
+
+# pruebas unitarias
+# todos los tets: pytest ../tests/ -v
+# para ver los print durante la ejecucion: pytest ../tests/ -v -s
+# todos los tests de routes: pytest ../tests/routes/ -v
+# todos los tests de controllers: pytest ../tests/controllers/ -v
+# todos los tests de services: pytest ../tests/services/ -v
